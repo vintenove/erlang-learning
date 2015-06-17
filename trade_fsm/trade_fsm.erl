ea518e7e32ef783f51bd564c7cc1bae245b26e1a
Marco Vintenove <marcovintenove@gmail.com>
2015-06-17 22:51:58 +0800

    Add FSM to FSM functions to the trade example
    Powered by 'Please concentrate for 5 mins'

--- trade_fsm/trade_fsm.erl
+++ trade_fsm/trade_fsm.erl
@@ -5,12 +5,6 @@
 -export([start/1, start_link/1, trade/2, accept_trade/1,
  make_offer/2, retract_offer/2, ready/1, cancel/1]).
 
- %% gen_fsm callbacks
- -export([init/1, handle_event/3, handle_sync_event/4,
- 	handle_info/3, terminate/3, code_change/4,
- 	idle/2, idle/3, idle_wait/3, negotiate/2, negotiate/3, wait/2,
- 	ready/2, ready/3]).
-
  %%% Public API
  start(Name) ->
  	gen_fsm:start(?MODULE, [Name], []).
@@ -42,3 +36,66 @@ ready(OwnPid) ->
 %% Cancel the transaction
 cancel(OwnPid) ->
 	gen_fsm:sync_send_event(OwnPid, cancel).
+
+%%%%%%%%  FSM TO FSM %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+
+%% Ask the FMS's pid for a trade session
+ask_negotiate(OtherPid, OwnPid) ->
+	gen_fsm:send_event(OtherPid, {ask_negotiate, OwnPid}).
+
+%% Forward the client message accepting the transaction
+accept_negotiate(OtherPid, OwnPid) ->
+	gen_fsm:send_event(OtherPid, {accept_negotiate, OwnPid}).
+
+%% Forward a client's offer
+do_offer(OtherPid, Item) ->
+	gen_fsm:send_event(OtherPid, {do_offer, Item}).
+
+%% Forward a client's cancellation
+undo_offer(OtherPid, Item) ->
+	gen_fsm:send_event(OtherPid, {undo_offer, Item}).
+
+%% Ask the other side if he's ready to trade
+are_you_ready(OtherPid) ->
+	gen_fsm:send_event(OtherPid, are_you_ready).
+
+%% Reply that we are not ready yet
+not_yet(OtherPid) ->
+	gen_fsm:send_event(OtherPid, not_yet).
+
+%% Reply that we are ready
+am_ready(OtherPid) ->
+	gen_fsm:send_event(OtherPid, 'ready!').
+
+%% Acknowledge that the fsm is in ready state
+ack_trans(OtherPid) ->
+	gen_fsm:send_event(OtherPid, ack).
+
+%% Ask if ready to commit
+ask_commit(OtherPid) ->
+	gen_fsm:send_event(OtherPid, ask_commit).
+
+%% Begin synchronous commit
+do_commit(OtherPid) ->
+	gen_fsm:sync_send_event(OtherPid, do_commit).
+
+%% Notify cancellation to the other FSM
+notifify_cancel(OtherPid) ->
+	gen_fsm:send_all_state_event(OtherPid, cancel).
+
+
+%% GEN_FSM CALLBACKS %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+-export([init/1, handle_event/3, handle_sync_event/4,
+	handle_info/3, terminate/3, code_change/4,
+	idle/2, idle/3, idle_wait/3, negotiate/2, negotiate/3, wait/2,
+	ready/2, ready/3]).
+
+-record(state, {name="",
+    other,
+    ownItems=[],
+    otherItems=[],
+    monitor,
+    from}).
+
+init(Name) ->
+	{ok, idle, #state{name=Name}}.
