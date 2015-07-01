8fe5ebb2fe237f3510e71819ea28ce1c99137b95
Marco Vintenove <marcovintenove@gmail.com>
2015-07-01 22:34:19 +0800

    musicians: finish it

--- music_band/musicians.erl
+++ music_band/musicians.erl
@@ -7,7 +7,7 @@
 -record(state, {name="", role, skill=good}).
 -define(DELAY, 750).
 
-start_link(Role, Skill) ->
+start_link({Role, Skill}) ->
 	gen_server:start_link({local, Role}, ?MODULE, [Role, Skill], []).
 
 stop(Role) ->
@@ -21,7 +21,7 @@ init([Role, Skill]) ->
 	TimeToPlay = random:uniform(3000),
 	Name = pick_name(),
 	StrRole = atom_to_list(Role),
-	io_format("Musician ~s, playing the ~s entered the room", [Name, StrRole]),
+	io:format("Musician ~s, playing the ~s entered the room", [Name, StrRole]),
 	{ok, #state{name=Name, role=StrRole, skill=Skill}, TimeToPlay}.
 
 pick_name() ->
@@ -35,7 +35,7 @@ firstnames() ->
 lastnames() ->
 	["Vintenove", "Loira", "Gomez", "Otero", "Ma", "Hsu", "Trevijano", "Raskolnikov", "Oblomov", "Russell"].
 
-handle_call(stop, _From, S=#state{}) ->
+handle_call(stop, _From, S = #state{}) ->
 	{stop, normal, ok, S};
 
 handle_call(_Message, _From, S) ->
@@ -43,3 +43,41 @@ handle_call(_Message, _From, S) ->
 
 handle_cast(_Message, S) ->
 	{noreply, S, ?DELAY}.
+
+handle_info(timeout, S = #state{name=N, skill=good}) ->
+	io:format("~s produced sound!~n", [N]),
+	{noreply, S, ?DELAY};
+
+handle_info(timeout, S = #state{name=N, skill=bad}) ->
+	case random:uniform(5) of
+		1 ->
+			io:format("~s mess up!~n", [N]),
+			{stop, bad_note, S};
+		_ ->
+			io:format("~s produced sound!~n", [N]),
+			{noreply, S, ?DELAY}
+	end;
+
+handle_info(_Message, S) ->
+	{noreply, S, ?DELAY}.
+
+code_change(_OldSource, State, _Extra) ->
+	{ok, State}.
+
+terminate(normal, S) ->
+	io:format("~s left the room! (~s)~n",
+		[S#state.name, S#state.role]);
+
+terminate(bad_note, S) ->
+	io:format("~s sucks!!! kick ~s out! ~n",
+		[S#state.name, S#state.role]);
+
+terminate(shutdown, S) ->
+	io:format("The last band member ~s was also kicked! (~s) ~n",
+		[S#state.name, S#state.role]);
+
+terminate(_Else, S) ->
+	io:format("~s sucks!!! kick ~s out! ~n",
+		[S#state.name, S#state.role]).
+
+
