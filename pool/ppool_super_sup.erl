4b1242411acfbbf094002256083d6145051ae2e2
Marco Vintenove <marcovintenove@gmail.com>
2015-08-03 23:26:57 +0200

    pool: 5 mins due

--- pool/ppool_super_sup.erl
+++ pool/ppool_super_sup.erl
@@ -15,8 +15,17 @@ stop() ->
 		_ -> ok
 	end.
 
-init([]]) ->
+init([]) ->
 	MaxRestart = 6,
 	MaxTime = 3600,
 	{ok, {{one_for_one, MaxRestart, MaxTime}, []}}.
+
+start_pool(Name, Limit, MFA) ->
+	ChildSpec = {Name, {ppool_sup, start_link, [Name, Limit, MFA]},
+	permanent, 10500, supervisor, [ppool_sup]},
+	supervisor:start_child(ppool, ChildSpec).
+
+stop_pool(Name) ->
+	supervisor:terminate_child(ppool, Name),
+	supervisor:delete_child(ppool, Name).
 
