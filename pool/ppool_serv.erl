3cd45dd2b5a58c5872e75d3ae44ec89f4db241d3
Marco Vintenove <marcovintenove@gmail.com>
2015-08-05 23:25:20 +0200

    pool : almost done with ppool_serv
    TODO: implement workers

--- pool/ppool_serv.erl
+++ pool/ppool_serv.erl
@@ -5,6 +5,19 @@
 
 -export([init/1, handle_call/3, handle_cast/2, handle_info/2, code_change/3, terminate/2]).
 
+-define(SPEC(MFA),
+	{worker_sup,
+	{ppool_worker_sup, start_link, [MFA]},
+	permanent,
+	10000,
+	supervisor,
+	[ppool_worker_sup]}).
+
+-record(state, {limit = 0,
+	sup,
+	refs,
+	queue = queue:new()}).
+
 start(Name, Limit, Sup, MFA) when is_atom(Name), is_integer(Limit) ->
 	gen_server:start({local, Name}, ?MODULE, {Limit, MFA, Sup}, []).
 
@@ -22,3 +35,61 @@ async_queue(Name, Args) ->
 
 stop(Name) ->
 	gen_server:call(Name, stop).
+
+%%%%%%%%%%%
+
+init({Limit, MFA, Sup}) ->
+	self() ! {start_worker_supervisor, Sup, MFA},
+	{ok, #state{limit=Limit, refs=gb_sets:empty()}}.
+
+handle_info({'DOWN', Ref, process, _Pid, _},
+	S = #state{limit=L, refs=R, sup=Sup}) ->
+	io:format("received down message"),
+	case gb_sets:is_element(Ref) of
+		true ->
+			handle_down_worker(Ref, S);
+		false ->
+			{noreply, S}
+	end;
+
+handle_info({start_worker_supervisor, Sup, MFA}, S = #state{}) ->
+	{ok, Pid} = supervisor:start_child(Sup, ?SPEC(MFA)),
+	{ok, #state{Sup=Pid}};
+
+handle_info(Message, State) ->
+	io:format("Unkown message in server: ~p", [Message]),
+	{noreply, State}.
+
+handle_call({run, Args}, _From, S = #state{limit=N, sup=Sup, refs=R}) when N > 0 ->
+	{ok, Pid} = supervisor:start_child(Sup, Args),
+	Ref = erlang:monitor(process, Pid),
+	{reply, {ok,Pid}, S#state{limit=N-1, refs=gb_sets:add(R,Ref)}};
+
+handle_call({run, _Args}, _From, S) -> % w/o when clause, unlike eg
+	{reply, noalloc, S};
+
+handle_call({sync, Args}, _From, S = #state{limit=N, sup=Sup, refs=R}) when N > 0 ->
+	{ok, Pid} = supervisor:start_child(Sup, Args),
+	Ref = erlang:monitor(process, Pid),
+	{reply, {ok,Pid}, S#state{limit=N-1, refs=gb_sets:add(R,Ref)}};
+
+handle_call({sync, Args}, From, S#state{queue=Q}) -> % w/o when clause, unlike eg
+	{noreply, noalloc, S#state{queue=queue:in({From, Args}, Q)}};
+
+handle_call({stop, _From, State}) ->
+	{stop, normal, ok, State};
+
+handle_call(_Message, _From, State) ->
+	{noreply, State}.
+
+handle_cast({async, Args}, S = #state{limit=N, sup=Sup, refs=R}) when N > 0) ->
+	{ok, Pid} = supervisor:start_child(Sup, Args),
+	Ref = erlang:monitor(process, Pid),
+	{noreply, S#state{limit=N-1, refs=gb_sets:add(R,Ref)}};
+
+handle_cast({async, Args}, S = #state{queue=Q}) ->
+	{noreply, S#state{queue=queue:in(Args, Q)}};
+
+handle_cast(_Message, State) ->
+	{noreply, State}.
+
